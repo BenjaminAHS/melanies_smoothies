022f3e9d30fddda6b19ad17973d36e1f21a0e824
BenjaminAHS <60747922+BenjaminAHS@users.noreply.github.com>
2025-12-10 16:45:41 +0100

    Import pandas and display dataframe in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -16,6 +17,8 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
